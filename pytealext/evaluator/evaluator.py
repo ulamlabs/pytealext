52664ce37cd4d13fc27c6ee556d1c0d518c93e8a
Łukasz Ptak <lukasz@ulam.io>
2022-01-27 15:31:02 +0100

    Refactor evaluator
    Split line into op and args immediately

--- pytealext/evaluator/evaluator.py
+++ pytealext/evaluator/evaluator.py
@@ -98,34 +98,38 @@ def eval_teal(
             if return_stack:
                 return stack, slots
             return [stack[-1]], slots
-        if line == "dup":
+
+        line_s = line.split()
+        op = line_s[0]
+        args = line_s[1:]
+        if op == "dup":
             x = stack[-1]
             stack.append(stack[-1])
-        elif line == "dup2":
+        elif op == "dup2":
             x = stack[-2], stack[-1]
             stack.extend(x)
-        elif line == "pop":
+        elif op == "pop":
             stack.pop()
-        elif line == "mulw":
+        elif op == "mulw":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             ab = a * b
             stack.extend(split128(ab))
-        elif line == "addw":
+        elif op == "addw":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             ab = a + b
             stack.extend(split128(ab))
-        elif line == "bitlen":
+        elif op == "bitlen":
             a = stack.pop()
             if isinstance(a, bytes):
                 a = int.from_bytes(a, "big")
             stack.append(a.bit_length())
-        elif line == "divmodw":
+        elif op == "divmodw":
             divisor_lo = stack.pop()
             divisor_hi = stack.pop()
             dividend_lo = stack.pop()
@@ -143,54 +147,54 @@ def eval_teal(
             remainder = dividend % divisor
             stack.extend(split128(quotient))
             stack.extend(split128(remainder))
-        elif line == "|":
+        elif op == "|":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a | b)
-        elif line == "&":
+        elif op == "&":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a & b)
-        elif line == "^":
+        elif op == "^":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a ^ b)
-        elif line == "~":
+        elif op == "~":
             a = stack.pop()
             if not isinstance(a, int):
                 raise Panic("Invalid type", current_line)
             x = (INTEGER_SIZE - 1) ^ a
             stack.append(x)
-        elif line == "shl":
+        elif op == "shl":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a << b % 2**64)
-        elif line == "shr":
+        elif op == "shr":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a >> b)
-        elif line == "sqrt":
+        elif op == "sqrt":
             a = stack.pop()
             if not isinstance(a, int):
                 raise Panic("Invalid type", current_line)
             stack.append(isqrt(a))
-        elif line == "assert":
+        elif op == "assert":
             a = stack.pop()
             if not isinstance(a, int):
                 raise Panic("Invalid type", current_line)
             if a == 0:
                 raise AssertionFailed(current_line)
-        elif line == "/":
+        elif op == "/":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -199,7 +203,7 @@ def eval_teal(
                 raise Panic("Division by zero", current_line)
             x = a // b
             stack.append(x)
-        elif line == "%":
+        elif op == "%":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -208,13 +212,13 @@ def eval_teal(
                 raise Panic("Division by zero", current_line)
             x = a % b
             stack.append(x)
-        elif line == "!":
+        elif op == "!":
             a = stack.pop()
             if not isinstance(a, int):
                 raise Panic("Invalid type", current_line)
             x = int(not a)
             stack.append(x)
-        elif line == "+":
+        elif op == "+":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -223,7 +227,7 @@ def eval_teal(
                 raise Panic("Overflow", current_line)
             x = a + b
             stack.append(x)
-        elif line == "-":
+        elif op == "-":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -232,7 +236,7 @@ def eval_teal(
                 raise Panic("Underflow", current_line)
             x = a - b
             stack.append(x)
-        elif line == "*":
+        elif op == "*":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -241,7 +245,7 @@ def eval_teal(
                 raise Panic("Overflow", current_line)
             x = a * b
             stack.append(x)
-        elif line == "&&":
+        elif op == "&&":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -250,7 +254,7 @@ def eval_teal(
                 stack.append(1)
             else:
                 stack.append(0)
-        elif line == "||":
+        elif op == "||":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
@@ -259,43 +263,43 @@ def eval_teal(
                 stack.append(1)
             else:
                 stack.append(0)
-        elif line == ">":
+        elif op == ">":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(int(bool(a > b)))
-        elif line == ">=":
+        elif op == ">=":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(int(bool(a >= b)))
-        elif line == "<":
+        elif op == "<":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(int(bool(a < b)))
-        elif line == "<=":
+        elif op == "<=":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, int) or not isinstance(b, int):
                 raise Panic("Invalid type", current_line)
             stack.append(int(bool(a <= b)))
-        elif line == "==":
+        elif op == "==":
             b = stack.pop()
             a = stack.pop()
             if type(a) is not type(b):
                 raise Panic("Type mismatch", current_line)
             stack.append(int(bool(a == b)))
-        elif line == "!=":
+        elif op == "!=":
             b = stack.pop()
             a = stack.pop()
             if type(a) is not type(b):
                 raise Panic("Type mismatch", current_line)
             stack.append(int(bool(a != b)))
-        elif line == "select":
+        elif op == "select":
             c = stack.pop()
             b = stack.pop()
             a = stack.pop()
@@ -305,24 +309,24 @@ def eval_teal(
                 stack.append(b)
             else:
                 stack.append(a)
-        elif line == "swap":
+        elif op == "swap":
             b = stack.pop()
             a = stack.pop()
             stack.append(b)
             stack.append(a)
-        elif line == "app_global_get":
+        elif op == "app_global_get":
             key = stack.pop()
             if context is None:
                 raise Exception("app_global_get requires execution environment context")
             val = context.global_state.get(key, 0)
             stack.append(val)
-        elif line == "app_global_put":
+        elif op == "app_global_put":
             b = stack.pop()
             a = stack.pop()
             if context is None:
                 raise Exception("app_global_put requires execution environment context")
             context.global_state[a] = b
-        elif line == "log":
+        elif op == "log":
             val = stack.pop()
             if not isinstance(val, bytes):
                 raise Panic("log requires bytes value", current_line)
@@ -333,13 +337,13 @@ def eval_teal(
                 raise Panic("log size limit exceeded", current_line)
             if len(context.log) > MaxLogCalls:
                 raise Panic("log calls limit exceeded", current_line)
-        elif line == "itob":
+        elif op == "itob":
             val = stack.pop()
             if not isinstance(val, int):
                 raise Panic("itob requires integer value", current_line)
             val = val.to_bytes(8, "big")
             stack.append(val)
-        elif line == "btoi":
+        elif op == "btoi":
             val = stack.pop()
             if not isinstance(val, bytes):
                 raise Panic("btoi requires bytes value", current_line)
@@ -347,20 +351,20 @@ def eval_teal(
                 raise Panic("btoi requires bytes of length 8 or less", current_line)
             val = int.from_bytes(val, "big")
             stack.append(val)
-        elif line == "concat":
+        elif op == "concat":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, bytes) or not isinstance(b, bytes):
                 raise Panic("Invalid type", current_line)
             stack.append(a + b)
-        elif line == "extract3":
+        elif op == "extract3":
             c = stack.pop()
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, bytes) or not isinstance(b, int) or not isinstance(c, int):
                 raise Panic("Invalid type", current_line)
             stack.append(a[b:b + c])
-        elif line == "extract_uint16":
+        elif op == "extract_uint16":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, bytes) or not isinstance(b, int):
@@ -368,7 +372,7 @@ def eval_teal(
             if b + 2 > len(a):
                 raise Panic("Out of bounds", current_line)
             stack.append(int.from_bytes(a[b:b + 2], "big"))
-        elif line == "extract_uint32":
+        elif op == "extract_uint32":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, bytes) or not isinstance(b, int):
@@ -376,7 +380,7 @@ def eval_teal(
             if b + 4 > len(a):
                 raise Panic("Out of bounds", current_line)
             stack.append(int.from_bytes(a[b:b + 4], "big"))
-        elif line == "extract_uint64":
+        elif op == "extract_uint64":
             b = stack.pop()
             a = stack.pop()
             if not isinstance(a, bytes) or not isinstance(b, int):
@@ -385,57 +389,58 @@ def eval_teal(
                 raise Panic("Out of bounds", current_line)
             stack.append(int.from_bytes(a[b:b + 8], "big"))
         # provisional support for txna
-        elif line.startswith("txna ApplicationArgs"):
-            arg_index = int(line.split(" ")[-1])
-            if arg_index > len(context.txn.app_args):
-                raise Panic("txna ApplicationArgs index out of bounds", current_line)
-            stack.append(context.txn.app_args[arg_index])
-        elif " " in line:
-            op, arg = line.split(" ")
-            if op == "byte":
-                if arg[0] == '"' or arg[-1] == '"':
-                    arg = arg[1:-1]  # strip quotes
-                    arg = arg.encode("utf-8")
-                elif arg[0:2] == "0x":
-                    arg = arg[2:]
-                    arg = bytes.fromhex(arg)
-                else:
-                    raise Panic("byte requires string or hex value", current_line)
-                stack.append(arg)
-            elif op == "int":
-                x = int(arg)
-                if x < 0 or x >= INTEGER_SIZE:
-                    raise Panic(
-                        f"int expects non-negative integer smaller than {INTEGER_SIZE} (actual={x})", current_line
-                    )
-                stack.append(x)
-            elif op == "bnz":
-                cond = stack.pop()
-                if cond != 0:
-                    current_line = branch_targets[arg]
-            elif op == "bz":
-                cond = stack.pop()
-                if cond == 0:
-                    current_line = branch_targets[arg]
-            elif op == "b":
-                current_line = branch_targets[arg]
-            elif op == "cover":
-                nr = int(arg)  # get the number after the space
-                top = stack.pop()
-                if nr > len(stack):
-                    raise Panic(f"cover {nr} with stack size {len(stack)}", current_line)
-                stack.insert(-nr, top)
+        elif op == "txna":
+            if args[0] == "ApplicationArgs":
+                arg_index = args[1]
+                if arg_index > len(context.txn.app_args):
+                    raise Panic("txna ApplicationArgs index out of bounds", current_line)
+                stack.append(context.txn.app_args[arg_index])
             else:
-                if op == "store":
-                    i = int(arg)
-                    x = stack.pop()
-                    slots[i] = x
-                elif op == "load":
-                    i = int(arg)
-                    x = slots[i]
-                    stack.append(x)
-                else:
-                    raise Exception(f"Operation '{line}' is not supported by the simulator")
+                raise Exception("Unsupported txna expression")
+        elif op == "byte":
+            print(line)
+            print(op, args)
+            arg = args[0]
+            if arg[0] == '"' and arg[-1] == '"':
+                arg = arg[1:-1]  # strip quotes
+                arg = arg.encode("utf-8")
+            elif arg.startswith("0x"):
+                arg = arg[2:]
+                arg = bytes.fromhex(arg)
+            else:
+                raise Panic("byte requires string or hex value", current_line)
+            stack.append(arg)
+        elif op == "int":
+            x = int(args[0])
+            if x < 0 or x >= INTEGER_SIZE:
+                raise Panic(
+                    f"int expects non-negative integer smaller than {INTEGER_SIZE} (actual={x})", current_line
+                )
+            stack.append(x)
+        elif op == "bnz":
+            cond = stack.pop()
+            if cond != 0:
+                current_line = branch_targets[args[0]]
+        elif op == "bz":
+            cond = stack.pop()
+            if cond == 0:
+                current_line = branch_targets[args[0]]
+        elif op == "b":
+            current_line = branch_targets[args[0]]
+        elif op == "cover":
+            nr = int(args[0])
+            top = stack.pop()
+            if nr > len(stack):
+                raise Panic(f"cover {nr} with stack size {len(stack)}", current_line)
+            stack.insert(-nr, top)
+        elif op == "store":
+            i = int(args[0])
+            x = stack.pop()
+            slots[i] = x
+        elif op == "load":
+            i = int(args[0])
+            x = slots[i]
+            stack.append(x)
         else:
             raise Exception(f"Operation '{line}' is not supported by the simulator")
     return stack, slots
