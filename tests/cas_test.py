253fc48b151540f8862f69486d946bc01be773d4
Łukasz Ptak <StylishTriangles@users.noreply.github.com>
2022-05-26 17:44:33 +0200

    test: BytesMin, BytesMax

---
+++ tests/cas_test.py
@@ -0,0 +1,27 @@
+from hypothesis import given
+from hypothesis import strategies as st
+from pyteal import Bytes, Int, ScratchVar, Seq
+
+from pytealext import BytesMax, BytesMin
+from tests.helpers import compile_and_run
+
+
+@given(
+    lhs=st.binary(max_size=64),
+    rhs=st.binary(max_size=64),
+)
+def test_bytes_min_max(lhs: int, rhs: int):
+    mini = ScratchVar(slotId=1)
+    maxi = ScratchVar(slotId=2)
+    ast = Seq(mini.store(BytesMin(Bytes(lhs), Bytes(rhs))), maxi.store(BytesMax(Bytes(lhs), Bytes(rhs))), Int(1))
+
+    stack, slots = compile_and_run(ast)
+
+    lhs = int.from_bytes(lhs, byteorder="big")
+    rhs = int.from_bytes(rhs, byteorder="big")
+    expected_min = min(lhs, rhs)
+    expected_max = max(lhs, rhs)
+
+    assert stack == [1]
+    assert int.from_bytes(slots[1], "big") == expected_min
+    assert int.from_bytes(slots[2], "big") == expected_max
